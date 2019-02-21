a1d209c9143ef668cf2ff5cf43e1f39eda174bfc
John Knickerbocker <PokeMaster843@gmail.com>
2019-02-21 11:50:29 -0500

    Update GameBoyCore.js

--- js/GameBoyCore.js
+++ js/GameBoyCore.js
@@ -4766,7 +4766,7 @@ GameBoyCore.prototype.initBootstrap = function () {
 GameBoyCore.prototype.ROMLoad = function () {
 	//Load the first two ROM banks (0x0000 - 0x7FFF) into regular gameboy memory:
 	this.ROM = [];
-	this.usedBootROM = settings[1] && ((!settings[11] && this.GBCBOOTROM.length == 0x800) || (settings[11] && this.GBBOOTROM.length == 0x100));
+	this.usedBootROM = settings[1] && ((!settings[11] && this.GBCBOOTROM.length >= 0x800) || (settings[11] && this.GBBOOTROM.length == 0x100));
 	var maxLength = this.ROMImage.length;
 	if (maxLength < 0x4000) {
 		throw(new Error("ROM image size too small."));
